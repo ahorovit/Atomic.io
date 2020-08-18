6036b64560e50510ec06bdf3ac135fabc1f556b1
Adin Horovitz <adin@tapclicks.com>
2020-08-17 19:10:03 -0700

    WIP: IdentityDetailActivity, add Identity type VectorDrawables

---
+++ app/src/main/java/com/drspaceman/atomicio/ui/IdentityDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.drspaceman.atomicio.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.drspaceman.atomicio.R
+
+class IdentityDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_identity_detail)
+    }
+}
