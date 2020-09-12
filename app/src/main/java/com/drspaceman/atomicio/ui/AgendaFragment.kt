89963ad053fe9f2e0424048147186106850f43da
Adin Horovitz <adin@tapclicks.com>
2020-09-11 19:49:19 -0700

    DayView is rendering

--- app/src/main/java/com/drspaceman/atomicio/ui/AgendaFragment.kt
+++ app/src/main/java/com/drspaceman/atomicio/ui/AgendaFragment.kt
@@ -1,15 +1,18 @@
 package com.drspaceman.atomicio.ui
 
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import com.drspaceman.atomicio.R
-import com.drspaceman.atomicio.adapter.HabitRecyclerViewAdapter
 import com.drspaceman.atomicio.viewmodel.BaseViewModel
 import com.drspaceman.atomicio.viewmodel.HabitPageViewModel
-
 import kotlinx.android.synthetic.main.fragment_agenda.*
+import java.text.DateFormat
 import java.util.*
+
 
 class AgendaFragment : BasePageFragment() {
     override val layoutId: Int = R.layout.fragment_agenda
@@ -20,66 +23,52 @@ class AgendaFragment : BasePageFragment() {
     private var habitSequence: MutableList<HabitPageViewModel.HabitViewData>? = null
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        // Create a new calendar object set to the start of today
+
+        // Create a new calendar object set to the start of today
+        val day = Calendar.getInstance()
+        day.set(Calendar.HOUR_OF_DAY, 0)
+        day.set(Calendar.MINUTE, 0)
+        day.set(Calendar.SECOND, 0)
+        day.set(Calendar.MILLISECOND, 0)
+
+        val timeFormat = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault())
+
+
+        // Inflate a label view for each hour the day view will display
+        val hour = day.clone() as Calendar
+        val hourLabelViews: MutableList<View> = ArrayList()
+        for (i in dayView.startHour..dayView.endHour) {
+            hour[Calendar.HOUR_OF_DAY] = i
+            val hourLabelView =
+                layoutInflater.inflate(R.layout.hour_label, dayView, false) as TextView
+            hourLabelView.setText(timeFormat.format(hour.time))
+            hourLabelViews.add(hourLabelView)
+        }
+        dayView.setHourLabelViews(hourLabelViews)
+    }
+
+
+
     override fun getEditDialogFragment(id: Long?): BaseDialogFragment {
         TODO("Not yet implemented")
     }
 
     override fun initializeRecyclerView() {
-        initializeData()
-        val sequence = habitSequence ?: return
-
-        habitSequenceRecyclerView.layoutManager = LinearLayoutManager(this.context)
-        recyclerViewAdapter = HabitRecyclerViewAdapter(sequence, this)
-        habitSequenceRecyclerView.adapter = recyclerViewAdapter
-
-//        attachItemTouchHelper()
+//        initializeData()
+//        val sequence = habitSequence ?: return
+//
+//        habitSequenceRecyclerView.layoutManager = LinearLayoutManager(this.context)
+//        recyclerViewAdapter = HabitRecyclerViewAdapter(sequence, this)
+//        habitSequenceRecyclerView.adapter = recyclerViewAdapter
+//
+////        attachItemTouchHelper()
         }
 
-
-
-    // @todo: implement DB and LiveData for habitSequence
-    private fun initializeData() {
-        habitSequence = mutableListOf(
-            HabitPageViewModel.HabitViewData(name="wake up", type="sleep"),
-            HabitPageViewModel.HabitViewData(name="eat breakfast", type="diet"),
-            HabitPageViewModel.HabitViewData(name="walk dog", type="exercise")
-        )
-    }
-
-    /**
-     * WIP: Make habitSequence items draggable for reordering
-     */
-    private fun attachItemTouchHelper() {
-        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
-            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
-            0
-        ) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                dragged: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-
-                habitSequence?.let {
-                    val positionDragged = dragged.adapterPosition
-                    val positionTarget = target.adapterPosition
-
-                    Collections.swap(it, positionDragged, positionTarget)
-
-                    recyclerViewAdapter?.notifyItemMoved(positionDragged, positionTarget)
-                }
-
-                return false
-            }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                TODO("Not yet implemented")
-            }
-
-        })
-
-        itemTouchHelper.attachToRecyclerView(habitSequenceRecyclerView)
-    }
 
     companion object {
         fun newInstance() = AgendaFragment()
